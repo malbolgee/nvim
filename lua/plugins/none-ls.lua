c11c6e7e272bc3d299161d688ec411d1299fdbe8
Malbolge <victor.gomes@icomp.ufam.edu.br>
2024-06-24 22:28:51 -0400

    feat: Add none-ls shell formatter
    This commit adds the shell formater to none-ls.
    
    Signed-off-by: Malbolge <victor.gomes@icomp.ufam.edu.br>

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,6 +6,7 @@ return {
       sources = {
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
+        null_ls.builtins.formatting.shfmt,
       },
     }
     vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
