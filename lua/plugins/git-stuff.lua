2d150f429891711b9bc587e62e62426ee641e3e6
Malbolge <victor.gomes@icomp.ufam.edu.br>
2024-06-27 13:24:49 -0400

    feat: Change gitsigns keymaps
    This commit changes some gitsigns keymaps:
    
     - preview_hunk
     - toggle_current_line_blame
    
    Signed-off-by: Malbolge <victor.gomes@icomp.ufam.edu.br>

--- lua/plugins/git-stuff.lua
+++ lua/plugins/git-stuff.lua
@@ -6,8 +6,8 @@ return {
     'lewis6991/gitsigns.nvim',
     config = function()
       require('gitsigns').setup()
-      vim.keymap.set('n', '<C-g><C-p>', ':Gitsigns preview_hunk<CR>', {})
-      vim.keymap.set('n', '<C-g><C-b>', ':Gitsigns toggle_current_line_blame<CR>', {})
+      vim.keymap.set('n', '<S-g><S-p>', ':Gitsigns preview_hunk<CR>', {})
+      vim.keymap.set('n', '<S-g><S-b>', ':Gitsigns toggle_current_line_blame<CR>', {})
     end,
   },
 }
